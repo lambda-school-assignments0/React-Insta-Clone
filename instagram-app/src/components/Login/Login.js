d380cb105f1650df75b2ab21322b525e13a8e023
schang1146 <sammychang2185@gmail.com>
2019-05-31 07:42:45 -0700

    styled login

--- instagram-app/src/components/Login/Login.js
+++ instagram-app/src/components/Login/Login.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+// import styling
+import './Login.css';
+
+// import image
+import InstagramLogo from '../../assets/instagram_logo_txt.png';
+
+// import reactstrap
+import { Button } from 'reactstrap'; // import button from reactstrap
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap'; // import card from reactstrap
+import { Container, Row, Col } from 'reactstrap'; // import layout from reactstrap
+import { Form, FormGroup, Label, Input, FormText } from 'reactstrap'; // import form
+
 class Login extends React.Component {
-    handleLogin = e => {
+    handleLogin = () => {
         let user = document.getElementsByName('username')[0].value;
         let pass = document.getElementsByName('password')[0].value;
 
@@ -11,11 +23,17 @@ class Login extends React.Component {
 
     render() {
         return (
-            <form className='Login' onSubmit={this.handleLogin}>
-                <input type='text' name='username' placeholder='username...'></input>
-                <input type='text' name='password' placeholder='password...'></input>
-                <button>Login</button>
-            </form>
+            <div className='Login'>
+                <Form>
+                    <form className='LoginForm' onSubmit={this.handleLogin}>
+                        <img src={InstagramLogo} alt='instagram-logo' />
+                        <Input type='text' name='username' id='Username' placeholder='Phone number, username, or email' />
+                        <Input type='password' name='password' id='Password' placeholder='Password'/>
+                        <Button color='primary'>Login</Button>
+                        <a href='#' className='LoginForgotPassword'>Forgot Password?</a>
+                    </form>
+                </Form>
+            </div>
         )
     }
 }
